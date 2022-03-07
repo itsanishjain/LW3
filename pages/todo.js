49b71814d7d37211031d5df307786e333a079561
anish jain <helloanishjain@gmail.com>
2022-03-07 12:14:32 +0530

    added code for usestat and useEffect

---
+++ pages/todo.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+
+import styles from '../styles/Todo.module.css'
+
+export default function Todo() {
+    const [task, setTask] = useState('hhh');
+    const [list, setList] = useState([]);  //
+
+
+
+    const [products, setProducts] = useState([]);
+
+
+    const fetchData = () => {
+        console.log("calling fetchData");
+        fetch('https://fakestoreapi.com/products')
+            .then(res => res.json())
+            .then(json => {
+                console.log(json)
+                setProducts(json)
+            })
+    }
+
+
+
+    useEffect(() => {
+        // fetchData();
+    }, [])
+
+    const handleChange = (e) => {
+        console.log(e)
+        setTask(e.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("submitting......");
+        setList([...list, task]);
+        console.log(list);
+        setTask('');
+    }
+
+    const handleDelete = (e, i) => {
+        e.preventDefault();
+        const l = list;
+        setList(list.filter((item, index) => index !== i));
+        console.log(list, "DEle");
+
+    }
+    return (
+        <div>
+            <form className={styles.mainFrom}>
+                <input value={task} onChange={handleChange} placeholder='add todo' />
+
+
+                <button onClick={handleSubmit}>Add Todo</button>
+                {
+                    list.length > 0 ? list.map((item, index) => (
+                        <div key={index}>
+                            {item}
+                            <button onClick={(e) => handleDelete(e, index)}>Delete</button>
+                        </div>
+                    )) : "NOothing...."
+                }
+            </form>
+
+            <p>haihhhh ---- {task}</p>
+        </div>
+    )
+}
